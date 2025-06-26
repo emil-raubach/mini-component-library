2007a4143fc11913470530118eccc394dc9bdd4e
Emil Raubach <eraubach@gmail.com>
2025-06-25 22:02:03 -0600

    Large progress bar with some styling.

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,49 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const sizes = {
+  small: { width: 370, height: 8 },
+  medium: { width: 370, height: 12 },
+  large: { width: 370, height: 24 },
 };
+
+const ProgressBar = ({ value, size }) => {
+  return (
+    <div>
+      <Label id="loadinglabel">Loading</Label>
+      <span
+        role="progressbar"
+        aria-labelledby="loadinglabel"
+        aria-valuenow={value}
+      >
+        <Container width={sizes.large.width} height={sizes.large.height}>
+          <rect
+            height={sizes.large.height}
+            width={sizes.large.width}
+            fill={COLORS.gray50}
+          />
+          <rect
+            height={sizes.large.height}
+            width={(sizes.large.width / 100) * value}
+            fill={COLORS.primary}
+          />
+        </Container>
+      </span>
+    </div>
+  );
+};
+
+const Label = styled.span`
+  font-size: 20px;
+  font-weight: 700;
+  margin-inline-end: 4rem;
+  text-align: center;
+`;
+
+const Container = styled.svg`
+  border-radius: 8px;
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  padding: 4px;
+`;
 
 export default ProgressBar;
